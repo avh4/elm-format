20001c0af05f34c3c8e3b1adaf4005449880faac
Aaron VonderHaar <gruen0aermel@gmail.com>
2015-09-14 22:59:29 -0700

    Use transducer pattern for expressions and patterns

--- src/Format.hs
+++ src/Format.hs
@@ -95,69 +95,69 @@ formatDefinition :: (String -> a -> a) -> E.Def -> a -> a
 formatDefinition write adef =
     case RA.drop adef of
         E.Definition pattern expr ->
-            write (formatPattern pattern)
+            formatPattern write pattern
             >- write " =\n    "
-            >- write (formatExpression expr)
+            >- formatExpression write expr
             >- write "\n"
         E.TypeAnnotation name typ ->
             write name
             >- write " : "
-            >- write (formatType typ)
+            >- formatType write typ
             >- write "\n"
 
 
-formatPattern :: P.RawPattern -> String
-formatPattern apattern =
+formatPattern :: (String -> a -> a) -> P.RawPattern -> a -> a
+formatPattern write apattern =
     case RA.drop apattern of
-        P.Data _ _ -> "<data>"
-        P.Record _ -> "<record>"
-        P.Alias _ _ -> "<alias>"
-        P.Var var -> var
-        P.Anything -> "<anything>"
-        P.Literal _ -> "<literal>"
+        P.Data _ _ -> write "<data>"
+        P.Record _ -> write "<record>"
+        P.Alias _ _ -> write "<alias>"
+        P.Var var -> write var
+        P.Anything -> write "<anything>"
+        P.Literal _ -> write "<literal>"
 
 
-formatExpression :: E.Expr -> String
-formatExpression aexpr =
+formatExpression :: (String -> a -> a) -> E.Expr -> a -> a
+formatExpression write aexpr =
     case RA.drop aexpr of
-        EG.Literal lit -> formatLiteral lit
-        EG.Var _ -> "<var>"
-        EG.Range _ _ -> "<range>"
-        EG.ExplicitList _ -> "<list>"
-        EG.Binop _ _ _ -> "<binop>"
-        EG.Lambda _ _ -> "<lambda>"
-        EG.App _ _ -> "<app>"
-        EG.If _ _ -> "<if>"
-        EG.Let _ _ -> "<let>"
-        EG.Case _ _ -> "<case>"
-        EG.Data _ _ -> "<data>"
-        EG.Access _ _ -> "<access>"
-        EG.Update _ _ -> "<update>"
-        EG.Record _ -> "<record>"
-        EG.Port _ -> "<port>"
-        EG.GLShader _ _ _ -> "<glshader>"
+        EG.Literal lit -> formatLiteral write lit
+        EG.Var _ -> write "<var>"
+        EG.Range _ _ -> write "<range>"
+        EG.ExplicitList _ -> write "<list>"
+        EG.Binop _ _ _ -> write "<binop>"
+        EG.Lambda _ _ -> write "<lambda>"
+        EG.App _ _ -> write "<app>"
+        EG.If _ _ -> write "<if>"
+        EG.Let _ _ -> write "<let>"
+        EG.Case _ _ -> write "<case>"
+        EG.Data _ _ -> write "<data>"
+        EG.Access _ _ -> write "<access>"
+        EG.Update _ _ -> write "<update>"
+        EG.Record _ -> write "<record>"
+        EG.Port _ -> write "<port>"
+        EG.GLShader _ _ _ -> write "<glshader>"
 
 
-formatLiteral :: L.Literal -> String
-formatLiteral lit =
+formatLiteral :: (String -> a -> a) -> L.Literal -> a -> a
+formatLiteral write lit =
     case lit of
-        L.IntNum _ -> "<int>"
-        L.FloatNum _ -> "<float>"
-        L.Chr _ -> "<char>"
-        L.Str s -> "\"" ++ s ++ "\"" -- TODO: quoting
-        L.Boolean _ -> "<boolean>"
+        L.IntNum _ -> write "<int>"
+        L.FloatNum _ -> write "<float>"
+        L.Chr _ -> write "<char>"
+        L.Str s -> write $ "\"" ++ s ++ "\"" -- TODO: quoting
+        L.Boolean _ -> write "<boolean>"
 
 
-formatType :: T.Raw -> String
-formatType atype =
+formatType :: (String -> a -> a) -> T.Raw -> a -> a
+formatType write atype =
     case RA.drop atype of
-        T.RLambda _ _ -> "<lambda>"
-        T.RVar var -> var -- TODO: not tested
-        T.RType var -> formatVar var
-        T.RApp _ _ -> "<app>"
-        T.RRecord _ _ -> "<record>"
+        T.RLambda _ _ -> write "<lambda>"
+        T.RVar var -> write var -- TODO: not tested
+        T.RType var -> formatVar write var
+        T.RApp _ _ -> write "<app>"
+        T.RRecord _ _ -> write "<record>"
 
 
-formatVar :: V.Raw -> String
-formatVar (V.Raw var) =
-    var
+formatVar :: (String -> a -> a) -> V.Raw -> a -> a
+formatVar write (V.Raw var) =
+    write var
