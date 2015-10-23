b1292e0dcfd0d3277ff372216eb41cbbb63929cf
Martin Janiczek <martin@janiczek.cz>
2015-10-23 09:39:07 +0200

    Take Cmd.yesOrNo from elm-package

---
+++ src/CommandLine/Helpers.hs
@@ -0,0 +1,13 @@
+module CommandLine.Helpers where
+
+import System.IO
+
+yesOrNo :: IO Bool
+yesOrNo =
+  do  hFlush stdout
+      input <- getLine
+      case input of
+        "y" -> return True
+        "n" -> return False
+        _   -> do putStr "Must type 'y' for yes or 'n' for no: "
+                  yesOrNo
