c4b48f5d56481f6c4b6105a9da35c978b3b0ddc3
Martin Janiczek <martin@janiczek.cz>
2015-10-23 09:48:12 +0200

    y/N -> y/n for consistency with elm-package

--- src/Main.hs
+++ src/Main.hs
@@ -68,7 +68,7 @@ getApproval autoYes filePath =
             putStrLn "This will overwrite the following files to use Elm’s preferred style:\n"
             putStrLn $ "  " ++ filePath ++ "\n"
             putStrLn "This cannot be undone! Make sure to back up these files before proceeding.\n"
-            putStr "Are you sure you want to overwrite these files with formatted versions? (y/N) "
+            putStr "Are you sure you want to overwrite these files with formatted versions? (y/n) "
             Cmd.yesOrNo
 
 main :: IO ()
