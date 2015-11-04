8d60f0aeac330bb3c86512579011b26895d3ae81
Aaron VonderHaar <gruen0aermel@gmail.com>
2015-11-03 23:04:57 -0800

    Add example HUnit test

--- tests/Test/Property.hs
+++ tests/Test/Property.hs
@@ -1,14 +1,16 @@
 module Test.Property where
 
 import Data.Char
+import Test.HUnit (assertEqual)
 import Test.Framework
+import Test.Framework.Providers.HUnit
 import Test.Framework.Providers.QuickCheck2
 import Test.QuickCheck
 
 
 propertyTests :: Test
 propertyTests =
-    testGroup "Parse/Print Agreement Tests"
-    [
-        testProperty "FOO" ((\s -> s == s) :: [Char] -> Bool)
+    testGroup "example test group"
+    [ testProperty "example QuickCheck test" ((\s -> s == s) :: [Char] -> Bool)
+    , testCase "example HUnit test" $ assertEqual "example assertion" 2 (1+1)
     ]
