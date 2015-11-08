8597f8d738de0a0b49c909a0b386fd34fd5e6f40
Aaron VonderHaar <gruen0aermel@gmail.com>
2015-11-08 10:33:17 -0800

    Remove dead code from Box.hs

--- src/Box.hs
+++ src/Box.hs
@@ -102,14 +102,6 @@ indent :: Box -> Box
 indent =
     mapLines (\l -> row [Tab, l])
 
--- boxMap :: (Line -> a) -> (Box -> a) -> Box -> a
--- boxMap singleFn boxFn b =
---     case b of
---         Single l ->
---             singleFn l
---         Stack _ ->
---             boxFn b
-
 
 -- TODO: should return Either [Box] [Line] -- replace isLine and destructure
 -- TODO: use destructure instead?
@@ -214,16 +206,6 @@ empty =
     Box' B.nullBox 0 False
 
 
-hspace :: Int -> Box'
-hspace c =
-    Box' (B.emptyBox 0 c) 0 (c > 0)
-
-
-vspace :: Int -> Box'
-vspace r =
-    Box' (B.emptyBox r 0) 0 (r > 0)
-
-
 text :: String -> Box'
 text s =
     Box' (B.text s) 0 (s /= "")
@@ -239,64 +221,14 @@ vbox =
     foldl vbox2 empty
 
 
-vboxlist :: Box' -> String -> Box' -> (a -> Box') -> [a] -> Box'
-vboxlist start mid end format items =
-    case items of
-        [] ->
-            hbox2 start end
-        (first:rest) ->
-            vbox $
-                [ hbox2margin start (format first) ]
-                ++ (List.map (hbox2margin (text mid) . format) rest) ++
-                if hasSize end then [ end ] else []
-
-
 hbox2 :: Box' -> Box' -> Box'
 hbox2 (Box' a _ ae) (Box' b _ be) =
     Box' (a <> b) 0 (ae || be)
 
 
-hbox2margin :: Box' -> Box' -> Box'
-hbox2margin (Box' a aMargin ae) (Box' b bMargin be) =
-    Box' (a <> b) (max aMargin bMargin) (ae || be)
-
-
 hbox :: [Box'] -> Box'
 hbox =
     foldl hbox2 empty
-
-
-hboxlist :: String -> String -> String -> (a -> Box') -> [a] -> Box'
-hboxlist start mid end format items =
-    hbox $
-        [ text start ]
-        ++ (List.map format items |> List.intersperse (text mid)) ++
-        [ text end ]
-
-
-hjoin :: Box' -> [Box'] -> Box'
-hjoin sep list =
-    hbox (List.intersperse sep list)
-
-
-indent' :: Int -> Box' -> Box'
-indent' i child =
-    hbox2margin (hspace i) child
-
-
-margin :: Int -> Box' -> Box'
-margin m (Box' child _ ae) =
-    Box' child m ae
-
-
-width :: Box' -> Int
-width (Box' b _ _) =
-    B.cols b
-
-
-height :: Box' -> Int
-height (Box' b _ _) =
-    B.rows b
 
 
 render :: Box' -> String
