7516b668ed4cfd6187cda82769eccbb589f4f8c1
Aaron VonderHaar <gruen0aermel@gmail.com>
2016-01-04 20:54:39 -0800

    Add RUnit and remove dead code from AST.Type

--- parser/src/AST/Type.hs
+++ parser/src/AST/Type.hs
@@ -1,14 +1,9 @@
 module AST.Type
     ( Type, Type'(..)
     , Port(..), getPortType
-    , fieldMap
-    , tuple
     ) where
 
-import qualified Data.Map as Map
-
 import qualified Reporting.Annotation as A
-import qualified Reporting.Region as R
 
 
 -- DEFINITION
@@ -18,7 +13,8 @@ type Type =
 
 
 data Type'
-    = RVar String
+    = RUnit
+    | RVar String
     | RTupleFunction Int -- will be 2 or greater, indicating the number of elements in the tuple
     | RApp Type [Type]
     | RTuple [Type]
@@ -38,16 +34,3 @@ getPortType portType =
   case portType of
     Normal tipe -> tipe
     Signal tipe _ -> tipe
-
-
-fieldMap :: [(String,a)] -> Map.Map String [a]
-fieldMap fields =
-  let add r (field,tipe) =
-        Map.insertWith (++) field [tipe] r
-  in
-      foldl add Map.empty fields
-
-
-tuple :: R.Region -> [Type] -> Type
-tuple region types =
-  A.A region (RTuple types)
