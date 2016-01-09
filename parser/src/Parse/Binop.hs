07f828cb32e8c7b2167035015f1ff7a3232de14e
Aaron VonderHaar <gruen0aermel@gmail.com>
2016-01-09 15:45:14 -0800

    Refactor Parse.Binop

--- parser/src/Parse/Binop.hs
+++ parser/src/Parse/Binop.hs
@@ -1,6 +1,5 @@
 module Parse.Binop (binops) where
 
-import qualified Data.List as List
 import Text.Parsec ((<|>), choice, try)
 
 import AST.V0_16
@@ -21,7 +20,12 @@ binops term last anyOp =
       e <- term
       ops <- nextOps
       sawNewline <- popNewlineContext
-      return $ A.drop $ split e ops sawNewline -- TODO: the main purpose of split is to merge the annotations, which it does incorrectly and is overridden here, so it should probably go away
+      return $
+        case ops of
+          [] ->
+            A.drop e
+          _ ->
+            E.Binops e ops sawNewline
   where
     nextOps =
       choice
@@ -35,35 +39,3 @@ binops term last anyOp =
                   Right e -> return [(preOpComments, op, preExpressionComments, e)]
         , return []
         ]
-
-
-split
-    :: E.Expr
-    -> [([Comment], Var.Ref, [Comment], E.Expr)]
-    -> Bool
-    -> E.Expr
-split e0 [] _ = e0
-split e0 ops multiline =
-    let
-        init :: A.Located (E.Expr,[([Comment], Var.Ref, [Comment], E.Expr)])
-        init = A.sameAs e0 (e0,[])
-
-        merge'
-          :: ([Comment], Var.Ref, [Comment], E.Expr)
-          -> A.Located x
-          -> (E.Expr,[([Comment], Var.Ref, [Comment], E.Expr)])
-          -> A.Located (E.Expr,[([Comment], Var.Ref, [Comment], E.Expr)])
-        merge' (po,o,pe,e) loc (e0,ops) =
-          A.merge e loc (e0,(po,o,pe,e):ops)
-
-        merge
-          :: ([Comment], Var.Ref, [Comment], E.Expr)
-          -> A.Located (E.Expr,[([Comment], Var.Ref, [Comment], E.Expr)])
-          -> A.Located (E.Expr,[([Comment], Var.Ref, [Comment], E.Expr)])
-        merge x loc =
-          merge' x loc (A.drop loc)
-
-        wrap :: (E.Expr,[([Comment], Var.Ref, [Comment], E.Expr)]) -> E.Expr'
-        wrap (e',ops) = E.Binops e' ops multiline
-    in
-      A.map wrap $ List.foldr merge init ops
