2ef7f4ad2c8e904ffbe87aa3bb5414147fe539e9
Aaron VonderHaar <gruen0aermel@gmail.com>
2016-01-19 19:55:02 -0800

    Add tests for type declarations

--- tests/test-files/good/AllSyntax/Declarations.elm
+++ tests/test-files/good/AllSyntax/Declarations.elm
@@ -1,6 +1,22 @@
 module AllSyntax.Declarations (..) where
 
 
+type DataType
+  = Ctor1
+  | Ctor2 ()
+  | Ctor3 (List ()) ()
+
+
+type DataTypeWithParams a b c
+  = Ctor1'
+  | Ctor2' a
+  | Ctor3' b c
+
+
+type alias TypeAlias =
+  { x : Int, y : Int }
+
+
 expressionDefinition _ _ =
   ()
 
