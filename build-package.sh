a636a4f91617321f78216b2e457078dfce607819
Aaron VonderHaar <gruen0aermel@gmail.com>
2016-01-29 22:24:36 -0800

    Update publishing instructions

--- build-package.sh
+++ build-package.sh
@@ -18,11 +18,50 @@ cabal configure
 cabal build
 cabal install
 
-tar zcvf elm-format-"$VERSION"-mac-x64.tgz -C .cabal-sandbox/bin elm-format
-keybase sign --detach-sign elm-format-"$VERSION"-mac-x64.tgz
+BUILD="elm-format-${VERSION}-mac-x64"
+
+tar zcvf "$BUILD".tgz -C .cabal-sandbox/bin elm-format
+keybase pgp sign --detached --infile "$BUILD".tgz --outfile "$BUILD".tgz.asc
 
 
 ## Build Windows x64 binary
 
 # cd package/win
 # see setup.txt
+# vagrant up
+## open VirtualBox and show the screen
+## Start PowerShell
+#># cd Desktop
+#># git fetch
+#># git checkout ${VERSION}
+#># cabal update
+#># cabal install --only-dependencies
+#># cabal clean
+#># cabal configure
+#># cabal build
+#># cabal install
+#># cp .\.cabal-sandbox\bin\elm-format.exe C:\vagrant
+## back to host computer
+# cd ./package/win/
+# vagrant halt
+
+
+## Build Linux x64 binary
+
+# cd package/linux
+# vagrant ssh
+#># bash /vagrant/build-package.sh
+# vagrant halt
+
+
+## Collect files
+
+# cd package/win
+# zip elm-format-${VERSION}-win-x64.zip elm-format.exe
+# cd ../..
+# mv package/win/elm-format-${VERSION}-win-x64.zip ../../
+# mv package/linux/elm-format-${VERSION}-linux-x64.tgz ./
+
+# for i in elm-format-${VERSION}-{mac-x64.tgz,win-x64.zip,linux-x64.tgz} do
+#   keybase pgp sign --detached --infile "$i" --outfile "$i".asc
+# end
