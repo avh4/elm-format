a615c11b327e8103960b9e8dcd6f14a020523915
Aaron VonderHaar <gruen0aermel@gmail.com>
2016-05-07 13:30:52 -0700

    Extract ElmFormat.Render.ElmStructure from Box

---
+++ src/ElmFormat/Render/ElmStructure.hs
@@ -0,0 +1,115 @@
+{-# OPTIONS_GHC -Wall #-}
+module ElmFormat.Render.ElmStructure
+  ( application, group, extensionGroup )
+  where
+
+
+import Elm.Utils ((|>))
+import Box
+
+import qualified Data.List as List
+
+{-|
+Formats as:
+
+    first rest0 rest1 rest2
+
+    first
+      rest0
+      rest1
+      rest2
+-}
+application :: Box -> [Box] -> Box
+application first rest =
+  case allSingles (first:rest) of
+    Right ls ->
+      ls
+        |> List.intersperse space
+        |> row
+        |> line
+
+    _ ->
+      stack1
+        $ first : (map indent rest)
+
+{-|
+`group True '<' ',' '>'` formats as:
+
+    <>
+
+    < child0 >
+
+    < child0; child1; child2 >
+
+    < child0
+    ; child1
+    ; child2
+    >
+-}
+group :: Bool -> String -> String -> String -> Bool -> [Box] -> Box
+group innerSpaces left sep right forceMultiline children =
+  case (forceMultiline, allSingles children) of
+    (_, Right []) ->
+      line $ row [punc left, punc right]
+
+    (False, Right ls) ->
+      line $ row $ concat
+        [ if innerSpaces then [punc left, space] else [punc left]
+        , List.intersperse (row [punc sep, space]) ls
+        , if innerSpaces then [space, punc right] else [punc right]
+        ]
+
+    _ ->
+      case children of
+        [] ->
+          line $ row [ punc left, punc right]
+
+        (first:rest) ->
+          stack1 $
+            (prefix (row [punc left, space]) first)
+            : (map (prefix $ row [punc sep, space]) rest)
+            ++ [ line $ punc right ]
+
+{-|
+Formats as:
+
+    { base | first }
+
+    { base | first, rest0, rest1 }
+
+    { base
+      | first
+      , rest0
+      , rest1
+    }
+-}
+extensionGroup :: Bool -> Box -> Box -> [Box] -> Box
+extensionGroup multiline base first rest =
+  case
+    ( multiline
+    , isLine base
+    , allSingles $ (first:rest)
+    )
+  of
+    (False, Right base', Right fields') ->
+      line $ row
+        [ punc "{"
+        , space
+        , base'
+        , space
+        , punc "|"
+        , space
+        , row (List.intersperse (row [punc ",", space]) fields')
+        , space
+        , punc "}"
+        ]
+
+    _ ->
+      stack1
+        [ prefix (row [punc "{", space]) base
+        , stack1
+            ([ prefix (row [punc "|", space]) first ]
+            ++ (map (prefix (row [punc ",", space])) rest))
+            |> indent
+        , line $ punc "}"
+        ]
---
+++ tests/ElmFormat/Render/ElmStructureTest.hs
@@ -0,0 +1,91 @@
+module ElmFormat.Render.ElmStructureTest where
+
+import Elm.Utils ((|>))
+
+import Test.HUnit (Assertion, assertEqual)
+import Test.Framework
+import Test.Framework.Providers.HUnit
+import qualified Data.Text.Lazy as LazyText
+import qualified Data.Text as Text
+
+import Box
+import ElmFormat.Render.ElmStructure
+
+
+trim :: String -> String
+trim text =
+    text
+        |> LazyText.pack
+        |> LazyText.lines
+        |> map LazyText.stripEnd
+        |> LazyText.unlines
+        |> LazyText.unpack
+
+
+assertLineOutput :: String -> Line -> Assertion
+assertLineOutput expected actual =
+    assertOutput (expected ++ "\n") (line actual)
+
+
+assertOutput :: String -> Box -> Assertion
+assertOutput expected actual =
+    assertEqual expected expected $
+        trim $ Text.unpack $ render $ actual
+
+
+word :: String -> Box
+word =
+    line . identifier
+
+
+block :: String -> Box
+block text =
+    stack1
+        [ line $ row [ w, w ]
+        , line $ row [ w, w ]
+        ]
+    where
+        w = identifier text
+
+
+tests :: Test
+tests =
+    testGroup "ElmFormat.Render.ElmStructure"
+    [ testCase "application (single line)" $
+        assertOutput "a b c\n" $
+            application (word "a" )
+                $ map word [ "b", "c" ]
+    , testCase "application (multiline)" $
+        assertOutput
+            ( unlines
+                [ "aa"
+                , "aa"
+                , "  bb"
+                , "  bb"
+                , "  c"
+                ]
+            ) $
+            application
+                ( block "a" )
+                [ block "b"
+                , line $ identifier "c"
+                ]
+    , testCase "group (empty)" $
+        assertOutput "()\n" $
+            group True "(" "," ")" False []
+    , testCase "group (single item, single line)" $
+        assertOutput "( foo )\n" $
+            group True "(" "," ")" False [ word "foo" ]
+    , testCase "group (single line)" $
+        assertOutput "( foo, bar )\n" $
+            group True "(" "," ")" False [ word "foo", word "bar" ]
+    , testCase "group (single line, no spaces)" $
+        assertOutput "(foo, bar)\n" $
+            group False "(" "," ")" False [ word "foo", word "bar" ]
+    , testCase "group (multiline)" $
+        assertOutput "( aa\n  aa\n, b\n, cc\n  cc\n)\n" $
+            group True "(" "," ")" False [ block "a", word "b", block "c" ]
+    , testCase "group (forced multiline)" $
+        assertOutput "( a\n, b\n, c\n)\n" $
+            group True "(" "," ")" True [ word "a", word "b", word "c" ]
+    ]
