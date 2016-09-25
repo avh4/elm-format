1e93c039bb27666636e4efcc9bc0095e3d55eda3
Bobby Priambodo <bobby.priambodo@gmail.com>
2016-09-25 10:36:27 +0700

    Add binwrappers, now this is installable

---
+++ package/npm/binwrap.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const spawn = require("child_process").spawn;
+
+const distDir = require('./config').distDir;
+
+module.exports = function(executable) {
+  var input = process.argv.slice(2);
+
+  const os = process.platform;
+
+  const extension = os === 'win32' ? '.exe' : '';
+  const filename = `${executable}${extension}`;
+
+  spawn(path.join(distDir, filename), input, { stdio: 'inherit' })
+    .on('exit', process.exit);
+};
