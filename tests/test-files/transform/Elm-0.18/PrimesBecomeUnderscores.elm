92b95a1ace9ab01c6fa32e9b26af509863f6285b
Aaron VonderHaar <gruen0aermel@gmail.com>
2016-10-27 19:09:27 -0700

    Convert primes to underscores in variable names

---
+++ tests/test-files/transform/Elm-0.18/PrimesBecomeUnderscores.elm
@@ -0,0 +1,9 @@
+declaration' : ()
+declaration' = ()
+
+typeVariable : number -> number' -> number'' -> number'
+typeVariable _ x _ = x
+
+parameter : () -> () -> () -> ()
+parameter x x' x'' = ()
+
---
+++ tests/test-files/transform/Elm-0.18/PrimesBecomeUnderscores.formatted.elm
@@ -0,0 +1,16 @@
+module Main exposing (..)
+
+
+declaration_ : ()
+declaration_ =
+    ()
+
+
+typeVariable : number -> number' -> number'' -> number'
+typeVariable _ x _ =
+    x
+
+
+parameter : () -> () -> () -> ()
+parameter x x_ x__ =
+    ()
