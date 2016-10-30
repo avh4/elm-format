3913276d5702b457faffece0d1a9467457623a09
Aaron VonderHaar <gruen0aermel@gmail.com>
2016-10-29 22:09:03 -0700

    Replace range syntax with List.range in Elm 0.18

---
+++ tests/test-files/transform/Elm-0.18/RangesBecomeListRange.elm
@@ -0,0 +1,11 @@
+range = [1..9]
+variableRange x y = [x.min..y]
+precommented = [{-A-}1..{-B-}2]
+precommented2 =
+    [ -- One
+      1
+    .. -- Two
+      2
+    ]
+commented = [{-A-}1{-B-}..{-C-}2{-D-}]
+needsParens = [f x..f y]
