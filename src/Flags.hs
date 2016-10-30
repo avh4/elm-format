3b6170a97867c9553f5d6e0f09b3f253e0b2f15e
Aaron VonderHaar <gruen0aermel@gmail.com>
2016-10-30 00:14:02 -0700

    Bump version to 0.5.0-alpha-dev-20161030

--- src/Flags.hs
+++ src/Flags.hs
@@ -104,7 +104,7 @@ helpInfo defaultVersion =
     top =
         concat
             [ "elm-format-" ++ show defaultVersion ++ " "
-            , showVersion This.version  ++ "-alpha-dev" ++ "\n"
+            , showVersion This.version  ++ "-alpha-dev-20161030" ++ "\n"
             ]
 
     examples =
