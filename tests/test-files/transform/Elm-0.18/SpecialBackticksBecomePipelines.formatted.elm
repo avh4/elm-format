9eaa885f4e0406b8cfadf7fb1764134b2c7823b9
Aaron VonderHaar <gruen0aermel@gmail.com>
2016-10-29 23:53:55 -0700

    Convert andThen and onError to pipelines in 0.18

---
+++ tests/test-files/transform/Elm-0.18/SpecialBackticksBecomePipelines.formatted.elm
@@ -0,0 +1,28 @@
+module Main exposing (..)
+
+
+andThenExample1 =
+    a |> andThen b |> andThen c
+
+
+qualifiedAndThen =
+    a |> Result.andThen b |> Json.Decode.andThen c
+
+
+onErrorExample1 =
+    a |> andThen b |> onError c
+
+
+withComments =
+    a {- 1 -} |> andThen {- 2 -} b {- 3 -} |> andThen {- 4 -} c
+
+
+andThenIn17 =
+    String.toInt "1234"
+        |> Result.andThen \year -> isValidYear year
+
+
+tasksIn17 =
+    Http.toTask (Http.getString "http://example.com/war-and-peace")
+        |> Task.andThen (\fullText -> Task.succeed (NewText fullText))
+        |> Task.onError (\error -> Task.succeed DidNotLoad)
