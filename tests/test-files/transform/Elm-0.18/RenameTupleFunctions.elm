6a39ec0f1209e06e61d8740c4455849ba7b82963
Aaron VonderHaar <gruen0aermel@gmail.com>
2016-11-13 19:50:32 -0800

    Upgrade fst, snd -> Tuple.first, Tuple.second

---
+++ tests/test-files/transform/Elm-0.18/RenameTupleFunctions.elm
@@ -0,0 +1,16 @@
+module Main exposing (..)
+
+
+foo =
+    [ fst ( 1, "Two" )
+    , snd ( 2, "One" )
+    , f fst snd
+    , Basics.fst ( 1, "Two" )
+    , Basics.snd ( 1, "Two" )
+    , ( f, "x" ) `fst` ()
+    , ( f, "x" ) `snd` ()
+    , ( f, "x" ) `Basics.fst` ()
+    , ( f, "x" ) `Basics.snd` ()
+    , Quux.fst ( 1, "Two" )
+    , Quux.snd ( 1, "Two" )
+    ]
