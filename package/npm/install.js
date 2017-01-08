a15d705eeb885463b7de92c37d7296206ad562d4
Richard Feldman <richard.t.feldman@gmail.com>
2017-01-07 18:09:18 -0800

    Don't include Elm version in package.json.

--- package/npm/install.js
+++ package/npm/install.js
@@ -4,9 +4,8 @@ var fs = require("fs");
 var packageInfo = require(path.join(__dirname, "package.json"));
 
 // Use major.minor.patch from version string - e.g. "1.2.3" from "1.2.3-alpha"
-var versions = packageInfo.version.match(/^(\d+\.\d+\.\d+)-elm(\d+\.\d+\.\d+).*$/);
-var binVersion = versions[1];
-var elmVersion = versions[2];
+var binVersion = packageInfo.version.match(/^(\d+\.\d+\.\d+).*$/)[1];
+
 
 // 'arm', 'ia32', or 'x64'.
 var arch = process.arch;
@@ -15,8 +14,8 @@ var arch = process.arch;
 var operatingSystem = process.platform;
 
 var filename = operatingSystem + "-" + arch + ".tar.gz";
-var url = "https://dl.bintray.com/elmlang/elm-format/"
-  + elmVersion + "/" + binVersion + "/" + filename;
+var url = "https://dl.bintray.com/elmlang/elm-format/default/"
+  + binVersion + "/" + filename;
 
 var binariesDir = path.join(__dirname, "binaries");
 var packageInfo = require(path.join(__dirname, "package.json"));
@@ -24,7 +23,7 @@ var binaryExtension = process.platform === "win32" ? ".exe" : "";
 var executablePaths = Object.keys(packageInfo.bin).map(function(executable) {
   return path.join(binariesDir, executable + binaryExtension);
 });
-var errorMessage = "Unfortunately, there are no elm-format " + binVersion + " binaries available for Elm " + elmVersion + " on your operating system and architecture.\n\nIf you would like to build Elm from source, there are instructions at https://github.com/elm-lang/elm-platform#build-from-source\n";
+var errorMessage = "Unfortunately, there are no elm-format " + binVersion + " binaries available on your operating system and architecture.\n\nIf you would like to build Elm from source, there are instructions at https://github.com/elm-lang/elm-platform#build-from-source\n";
 
 binstall(url, {path: binariesDir},
   {verbose: true, verify: executablePaths, errorMessage: errorMessage}
