6d56fbcacd7869306f729ecb9dfc5c84b208e50b
Aaron VonderHaar <gruen0aermel@gmail.com>
2017-03-06 08:58:58 -0800

    Consolidate Declaration tests

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -244,7 +244,7 @@ checkWaysToRun
 checkGood 0.16 Simple.elm
 checkGood 0.16 AllSyntax/0.16/AllSyntax.elm
 checkGoodAllSyntax 0.16 Module
-checkGoodAllSyntax 0.18 Declarations
+checkGood 0.18 AllSyntax/0.18/Declarations.elm
 checkGoodAllSyntax 0.18 Patterns
 checkGoodAllSyntax 0.18 Types
 checkGood 0.18 AllSyntax/0.18/Expressions.elm
