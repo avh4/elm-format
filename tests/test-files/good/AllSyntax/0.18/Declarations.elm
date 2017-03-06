6f34aedf8bb9e03e61ae679bbce6f59a0f85666e
Aaron VonderHaar <gruen0aermel@gmail.com>
2017-03-06 08:58:58 -0800

    Consolidate Declaration tests

--- tests/test-files/good/AllSyntax/0.18/Declarations.elm
+++ tests/test-files/good/AllSyntax/0.18/Declarations.elm
@@ -1,19 +1,82 @@
 module AllSyntax.Declarations exposing (..)
 
+--
+-- Union type
+--
 
-type DataType
+
+type DataType1
     = Ctor1
     | Ctor2 ()
     | Ctor3 (List ()) ()
 
 
-type DataTypeWithParams a b c
+type {- A -} DataType2 {- B -}
+    = {- C -} Ctor1 {- D -}
+    | {- E -} Ctor2 {- F -} () {- G -}
+    | {- H -} Ctor3 {- I -} (List ()) {- J -} ()
+
+
+type
+    --A
+    DataType3
+    --B
+    = --C
+      Ctor1
+      --D
+    | --E
+      Ctor2
+        --F
+        ()
+      --G
+    | --H
+      Ctor3
+        --I
+        (List ())
+        --J
+        ()
+
+
+
+--
+-- Union type with params
+--
+
+
+type DataTypeWithParams1 a b c
     = Ctor1'
     | Ctor2' a
     | Ctor3' b c
 
 
-type alias TypeAlias a b =
+type {- K -} DataTypeWithParams2 {- L -} a {- M -} b {- N -} c {- O -}
+    = Ctor1'
+    | Ctor2' a
+    | Ctor3' b c
+
+
+type
+    --K
+    DataTypeWithParams3
+        --L
+        a
+        --M
+        b
+        --N
+        c
+    --O
+    = Ctor1'
+    | Ctor2' a
+    | Ctor3' b c
+
+
+
+--
+-- Type alias
+--
+
+
+type alias TypeAlias1 a b =
     { x : Int, y : Int, z : ( a, b ) }
 
 
@@ -26,12 +89,77 @@ type alias
     ()
 
 
+type {- P -} alias {- Q -} TypeAlias2 {- R -} a {- S -} b {- T -} =
+    {- U -}
+    { x : Int, y : Int, z : ( a, b ) }
+
+
+type
+    --P
+    alias
+    --Q
+    TypeAlias3
+        --R
+        a
+        --S
+        b
+    --T
+    =
+    --U
+    { x : Int, y : Int, z : ( a, b ) }
+
+
+
+--
+-- Expressions
+--
+
+
 expressionDefinition _ _ =
     ()
 
 
 expressionDefinitionNoPatterns =
     ()
+
+
+expressionTypeAnnotation1 : ()
+expressionTypeAnnotation1 =
+    ()
+
+
+expressionTypeAnnotation2 {- V -} : {- W -} ()
+expressionTypeAnnotation2 =
+    ()
+
+
+expressionTypeAnnotation3 --V
+    :
+    --W
+    ()
+expressionTypeAnnotation3 =
+    ()
+
+
+expressionTypeAnnotation4 :
+    --AD
+    ()
+expressionTypeAnnotation4 =
+    ()
+
+
+expressionTypeAnnotationForcedMultiline :
+    Int
+    -> Result String Bool
+    -> List (Maybe ())
+expressionTypeAnnotationForcedMultiline _ _ =
+    []
+
+
+
+--
+-- Declarations in let expressions
+--
 
 
 expressionDefinitionsInLet =
@@ -45,14 +173,37 @@ expressionDefinitionsInLet =
         ()
 
 
-expressionTypeAnnotation : ()
-expressionTypeAnnotation =
-    ()
+expressionDefinitionsInLet2 =
+    let
+        {- X -}
+        def1 =
+            {- Z -}
+            ()
+
+        {- AA -}
+        def2 =
+            {- AC -}
+            ()
+
+        {- AD -}
+    in
+        {- AE -}
+        ()
 
 
-expressionTypeAnnotationForcedMultiline :
-    Int
-    -> Result String Bool
-    -> List (Maybe ())
-expressionTypeAnnotationForcedMultiline _ _ =
-    []
+expressionDefinitionsInLet3 =
+    let
+        --X
+        def1 =
+            --Y
+            ()
+
+        --Z
+        def2 =
+            --AA
+            ()
+
+        --AB
+    in
+        --AC
+        ()
