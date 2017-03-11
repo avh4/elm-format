d8980aa926ce2802bc632548d6ff478d1f947886
Aaron VonderHaar <gruen0aermel@gmail.com>
2017-03-11 14:49:47 -0800

    Replace filemanip with FilePather, since the latter doesn't depend on unix-compat

--- src/ElmFormat/Filesystem.hs
+++ src/ElmFormat/Filesystem.hs
@@ -1,11 +1,23 @@
 module ElmFormat.Filesystem where
 
-import System.FilePath.Find (find, fileName, (/=?), (==?), extension)
+import System.FilePath.FilePather.Find (findp)
+import System.FilePath.FilePather.FilterPredicate (filterPredicate)
+import System.FilePath.FilePather.RecursePredicate (recursePredicate)
+import Data.Text (pack, isSuffixOf)
+
+hasExtension :: String -> FilePath -> fileType -> Bool
+hasExtension ext path _ =
+    isSuffixOf (pack ext) (pack path)
+
+
+hasntFilename :: String -> FilePath -> Bool
+hasntFilename name path =
+      not $ (isSuffixOf (pack $ '/' : name) (pack path) || name == path)
 
 
 findAllElmFiles :: FilePath -> IO [FilePath]
 findAllElmFiles inputFile =
-    find
-        (fileName /=? "elm-stuff") -- TODO: not tested
-        (extension ==? ".elm")
+    findp
+        (filterPredicate $ hasExtension ".elm")
+        (recursePredicate $ hasntFilename "elm-stuff") -- TODO: not tested
         inputFile
