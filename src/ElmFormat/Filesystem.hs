2135d7effc6f54d04f549d388c8080912ff39179
Aaron VonderHaar <gruen0aermel@gmail.com>
2017-03-11 18:17:35 -0800

    Remove dependency on FilePather

--- src/ElmFormat/Filesystem.hs
+++ src/ElmFormat/Filesystem.hs
@@ -1,23 +1,47 @@
 module ElmFormat.Filesystem where
 
-import System.FilePath.FilePather.Find (findp)
-import System.FilePath.FilePather.FilterPredicate (filterPredicate)
-import System.FilePath.FilePather.RecursePredicate (recursePredicate)
+import System.Directory (listDirectory, doesDirectoryExist)
+import System.FilePath ((</>))
 import Data.Text (pack, isSuffixOf)
 
-hasExtension :: String -> FilePath -> fileType -> Bool
-hasExtension ext path _ =
+
+collectFiles :: Monad m => (a -> m [a]) -> a -> m [a]
+collectFiles children root =
+    do
+        xs <- children root
+        subChildren <- mapM (collectFiles children) xs
+        return $ root : concat subChildren
+
+
+listDir :: FilePath -> IO [FilePath]
+listDir path =
+    fmap (map (path </>)) $ listDirectory path
+
+
+fileList :: FilePath -> IO [FilePath]
+fileList =
+  let
+      children path =
+          if hasntFilename "elm-stuff" path then
+              do
+                  directory <- doesDirectoryExist path
+                  if directory then listDir path else return []
+          else
+              return []
+  in
+      collectFiles children
+
+
+hasExtension :: String -> FilePath -> Bool
+hasExtension ext path =
     isSuffixOf (pack ext) (pack path)
+
+
+findAllElmFiles :: FilePath -> IO [FilePath]
+findAllElmFiles inputFile =
+    filter (hasExtension ".elm") <$> fileList inputFile
 
 
 hasntFilename :: String -> FilePath -> Bool
 hasntFilename name path =
       not $ (isSuffixOf (pack $ '/' : name) (pack path) || name == path)
-
-
-findAllElmFiles :: FilePath -> IO [FilePath]
-findAllElmFiles inputFile =
-    findp
-        (filterPredicate $ hasExtension ".elm")
-        (recursePredicate $ hasntFilename "elm-stuff") -- TODO: not tested
-        inputFile
