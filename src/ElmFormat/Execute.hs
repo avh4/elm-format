002f0e7b372239413549dbf4aa2e08ca46203e9e
Aaron VonderHaar <gruen0aermel@gmail.com>
2017-03-14 22:45:16 -0700

    Extract ElmFormat.Execute.Program

--- src/ElmFormat/Execute.hs
+++ src/ElmFormat/Execute.hs
@@ -1,16 +1,41 @@
-module ElmFormat.Execute (forHuman, forMachine, forMachineInit, forMachineDone) where
+{-# LANGUAGE Rank2Types #-}
+module ElmFormat.Execute (forHuman, forMachine, run) where
 
 {-| This module provides executors that can take streams of Operations and
 perform IO.
 -}
 
+import Prelude hiding (init)
+import Elm.Utils ((|>))
 import Control.Monad.State
+import Control.Monad.Free
 import ElmFormat.Operation
 import ElmVersion
 
 import qualified ElmFormat.FileStore as FileStore
 import qualified Messages.Formatter.HumanReadable as HumanReadable
 import qualified Messages.Formatter.Json as Json
+
+
+data Program opF state = Program
+    { init :: (IO (), state)
+    , step :: forall a. opF a -> StateT state IO a
+    , done :: state -> IO ()
+    }
+
+
+run :: Program opF state -> Free opF a -> IO a
+run program operations =
+    do
+        let Program init step done = program
+        let (initIO, initState) = init
+        initIO
+        (result, finalState) <-
+            operations
+                |> foldFree step
+                |> flip runStateT initState
+        done finalState
+        return result
 
 
 {-| Execute Operations in a fashion appropriate for interacting with humans. -}
@@ -23,19 +48,14 @@ forHuman operation =
 
 
 {-| Execute Operations in a fashion appropriate for use by automated scripts. -}
-forMachine :: ElmVersion -> OperationF a -> StateT Bool IO a
-forMachine elmVersion operation =
-    case operation of
-        InFileStore op -> lift $ FileStore.execute op
-        InInfoFormatter op -> Json.format elmVersion op
-        DeprecatedIO io -> lift io
-
-
-forMachineInit :: (IO (), Bool)
-forMachineInit =
-    Json.init
-
-
-forMachineDone :: IO ()
-forMachineDone =
-    Json.done
+forMachine :: ElmVersion -> Program OperationF Bool
+forMachine elmVersion =
+    Program
+        { init = Json.init
+        , step = \operation ->
+            case operation of
+                InFileStore op -> lift $ FileStore.execute op
+                InInfoFormatter op -> Json.format elmVersion op
+                DeprecatedIO io -> lift io
+        , done = const Json.done
+        }
