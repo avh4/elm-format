6489ff943e741a4e8969929e0cbbf80eeb3f17bd
Aaron VonderHaar <gruen0aermel@gmail.com>
2017-03-19 16:32:38 -0700

    Add tests for Windows EOL handling

---
+++ tests/test-files/transform/WindowsEol.elm
@@ -0,0 +1,15 @@
+module AllSyntax.WindowsEol exposing (..)
+
+{-| elm-format converts all CRLF line-endings to LF.
+
+CRs that are within strings will be escaped.
+-}
+
+
+multilineString =
+    """
+"""
+
+
+string = "
+"
