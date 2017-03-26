0fa68c32b0370ed0d85d982b9994474ff25f35cf
Aaron VonderHaar <gruen0aermel@gmail.com>
2017-03-25 22:05:44 -0700

    Point to existing binaries on github

--- package/npm/elmFormatDownload.js
+++ package/npm/elmFormatDownload.js
@@ -3,18 +3,32 @@ var path = require("path");
 exports.url = function(operatingSystem, arch) {
   // 'arm', 'ia32', or 'x64'.
   arch = arch || process.arch;
+  arch = {}[arch] || arch;
 
   // 'darwin', 'freebsd', 'linux', 'sunos' or 'win32'
   operatingSystem = operatingSystem || process.platform;
+  operatingSystem = {
+    darwin: "mac",
+    win32: "win"
+  }[operatingSystem] || operatingSystem;
+
+  var ext = {
+    win: "zip"
+  }[operatingSystem] || "tgz";
 
   var packageInfo = require(path.join(__dirname, "package.json"));
   var binVersion = packageInfo.version;
 
-  var filename = operatingSystem + "-" + arch + ".tar.gz";
-  var url = "https://dl.bintray.com/elmlang/elm-format/default/" +
+  var url = "https://github.com/avh4/elm-format/releases/download/" +
     binVersion +
-    "/" +
-    filename;
+    "/elm-format-0.18-" +
+    binVersion +
+    "-" +
+    operatingSystem +
+    "-" +
+    arch +
+    "." +
+    ext;
 
   return url;
 };
