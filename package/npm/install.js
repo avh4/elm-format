05c404d48a375d6c5db2e247ad45d1f19fd28d30
Aaron VonderHaar <gruen0aermel@gmail.com>
2017-03-25 21:16:48 -0700

    Format install.js

--- package/npm/install.js
+++ package/npm/install.js
@@ -5,7 +5,6 @@ var packageInfo = require(path.join(__dirname, "package.json"));
 
 var binVersion = packageInfo.version;
 
-
 // 'arm', 'ia32', or 'x64'.
 var arch = process.arch;
 
@@ -13,8 +12,10 @@ var arch = process.arch;
 var operatingSystem = process.platform;
 
 var filename = operatingSystem + "-" + arch + ".tar.gz";
-var url = "https://dl.bintray.com/elmlang/elm-format/default/"
-  + binVersion + "/" + filename;
+var url = "https://dl.bintray.com/elmlang/elm-format/default/" +
+  binVersion +
+  "/" +
+  filename;
 
 var binariesDir = path.join(__dirname, "binaries");
 var packageInfo = require(path.join(__dirname, "package.json"));
@@ -22,13 +23,20 @@ var binaryExtension = process.platform === "win32" ? ".exe" : "";
 var executablePaths = Object.keys(packageInfo.bin).map(function(executable) {
   return path.join(binariesDir, executable + binaryExtension);
 });
-var errorMessage = "Unfortunately, there are no elm-format " + binVersion + " binaries available on your operating system and architecture.\n\nIf you would like to build elm-format from source, there are instructions at https://github.com/avh4/elm-format#building-from-source\n";
+var errorMessage = "Unfortunately, there are no elm-format " +
+  binVersion +
+  " binaries available on your operating system and architecture.\n\nIf you would like to build elm-format from source, there are instructions at https://github.com/avh4/elm-format#building-from-source\n";
 
-binstall(url, {path: binariesDir},
-  {verbose: true, verify: executablePaths, errorMessage: errorMessage}
-).then(function(successMessage) {
+binstall(
+  url,
+  { path: binariesDir },
+  { verbose: true, verify: executablePaths, errorMessage: errorMessage }
+).then(
+  function(successMessage) {
     console.log(successMessage);
-  }, function(errorMessage) {
+  },
+  function(errorMessage) {
     console.error(errorMessage);
     process.exit(1);
-  });
+  }
+);
