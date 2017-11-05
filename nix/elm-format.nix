befe790b06e48cc109a5b96b4163f5788f130078
Anton Strömkvist <anton@stromkvist.com>
2017-11-05 02:22:39 +0100

    Add Nix build files

---
+++ nix/elm-format.nix
@@ -0,0 +1,29 @@
+{ mkDerivation, ansi-terminal, ansi-wl-pprint, base, binary
+, bytestring, Cabal, cmark, containers, coreutils, directory
+, filepath, free, git, HUnit, indents, json, mtl
+, optparse-applicative, parsec, process, QuickCheck, quickcheck-io
+, split, stdenv, tasty, tasty-golden, tasty-hunit, tasty-quickcheck
+, text
+}:
+mkDerivation {
+  pname = "elm-format";
+  version = "0.7.0";
+  src = ../.;
+  isLibrary = true;
+  isExecutable = true;
+  setupHaskellDepends = [ base Cabal directory filepath process ];
+  libraryHaskellDepends = [
+    ansi-terminal ansi-wl-pprint base binary bytestring containers
+    directory filepath free indents json mtl optparse-applicative
+    parsec process split text
+  ];
+  executableHaskellDepends = [ base ];
+  testHaskellDepends = [
+    base cmark containers HUnit mtl parsec QuickCheck quickcheck-io
+    split tasty tasty-golden tasty-hunit tasty-quickcheck text
+  ];
+  homepage = "http://elm-lang.org";
+  description = "A source code formatter for Elm";
+  license = stdenv.lib.licenses.bsd3;
+  buildTools = [ git coreutils ];
+}
