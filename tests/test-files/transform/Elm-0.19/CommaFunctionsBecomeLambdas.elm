0a626d246f4adb598e700a9843cabc17a616298a
Aaron VonderHaar <gruen0aermel@gmail.com>
2018-02-18 11:44:35 -0800

    0.19 upgrade: Convert all comma functions to lambdas

--- tests/test-files/transform/Elm-0.19/CommaFunctionsBecomeLambdas.elm
+++ tests/test-files/transform/Elm-0.19/CommaFunctionsBecomeLambdas.elm
@@ -1,3 +1,8 @@
 tuple2 = (,) 1 2
 tuple2_partial = (,) 1
 tuple2_function = (,)
+
+tuple3 = (,,) 1 2 3
+tuple3_partial1 = (,,) 1 2
+tuple3_partial2 = (,,) 1
+tuple3_function = (,,)
--- tests/test-files/transform/Elm-0.19/CommaFunctionsBecomeLambdas.formatted.elm
+++ tests/test-files/transform/Elm-0.19/CommaFunctionsBecomeLambdas.formatted.elm
@@ -11,3 +11,19 @@ tuple2_partial =
 
 tuple2_function =
     \a b -> ( a, b )
+
+
+tuple3 =
+    (\a b c -> ( a, b, c )) 1 2 3
+
+
+tuple3_partial1 =
+    (\a b c -> ( a, b, c )) 1 2
+
+
+tuple3_partial2 =
+    (\a b c -> ( a, b, c )) 1
+
+
+tuple3_function =
+    \a b c -> ( a, b, c )
