db0c784f54a8f3a76c8c9016e533320dd7f32c7b
Aaron VonderHaar <gruen0aermel@gmail.com>
2018-04-11 23:48:10 -0700

    0.19 upgrade: correctly convert uses of (!) in unusual binary expressions

--- tests/test-files/transform/Elm-0.19/RemoveBangOperator.elm
+++ tests/test-files/transform/Elm-0.19/RemoveBangOperator.elm
@@ -1,5 +1,14 @@
 batch model a b = model ! [ a, b ]
 batch_withComments model a b = model {- A -} ! {- B -} [ a, b ]
+batch_notALiteralList model cmds = model ! cmds
 
 none model = model ! []
 none_withComments model = model {- A -} ! {- B -} [ {- C -} ]
+
+inBinaryExpression model x a b = x + model ! [ a ] * [ b ]
+multipleBangs model x = x + model ! [] * model ! []
+nestedBangs model a b = model ! [ a ] ! [ b ] ! []
+withHighPrecedenceOperators_leftAssoc f g x = f >> g ! [] >> x
+withHighPrecedenceOperators_leftAssoc_complex a b c d e f g h i j = a >> b + c >> d >> e ! f >> g >> h + i >> j
+withHighPrecedenceOperators_rightAssoc f g x = f << g ! [] << x
+withFunctionApplication f g x = f x ! g x
