da24db2f2cd59f3a1c7abe3e81cfdf043f2ae8f4
Aaron VonderHaar <gruen0aermel@gmail.com>
2018-04-12 23:40:42 -0700

    0.19 upgrade: replace Basics.flip

---
+++ tests/test-files/transform/Elm-0.19/RemoveBasicsFlip.elm
@@ -0,0 +1,10 @@
+fullyApplied f = flip f "2" 1
+fullyApplied_comments f = flip{-A-}f{-B-}"2"{-C-}1
+partiallyApplied f = flip f "2"
+partiallyApplied_comments f = flip{-A-}f{-B-}"2"
+functionOnly f = flip f
+functionOnly_comments f = flip{-A-}f
+unapplied = flip
+extraArgs f = flip f 1 2 3 4 5
+
+qualified f = Basics.flip f "2" 1
