674a580fa7f7752af3405fddba01ddc7f7194554
Aaron VonderHaar <gruen0aermel@gmail.com>
2018-04-13 22:02:48 -0700

    0.19 upgrade: replace Basics.curry

---
+++ tests/test-files/transform/Elm-0.19/RemoveBasicsCurry.formatted.elm
@@ -0,0 +1,45 @@
+module Main exposing (..)
+
+
+fullyApplied f =
+    f ( "1", 2 )
+
+
+fullyApplied_comments f =
+    f {- A -} ( {- B -} "1", {- C -} 2 )
+
+
+partiallyApplied f =
+    \b -> f ( "1", b )
+
+
+partiallyApplied_comments f =
+    \b ->
+        {- A -}
+        f ( {- B -} "1", b )
+
+
+functionOnly f =
+    \a b -> f ( a, b )
+
+
+functionOnly_comments f =
+    \a b ->
+        {- A -}
+        f ( a, b )
+
+
+unapplied =
+    \f a b -> f ( a, b )
+
+
+extraArgs f =
+    f ( 1, 2 ) 3 4 5
+
+
+qualified f =
+    f ( "1", 2 )
+
+
+nested f =
+    f ( f ( 1, 2 ), 3 )
