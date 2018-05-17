32516ee0e94b311ed40df33c8ec7a6cce5e6599d
Aaron VonderHaar <gruen0aermel@gmail.com>
2018-05-16 22:21:28 -0700

    0.19 upgrade: keep all comments when upgrading Html.Attributes.style

--- tests/test-files/transform/Elm-0.19/ConvertHtmlAttributesStyle.elm
+++ tests/test-files/transform/Elm-0.19/ConvertHtmlAttributesStyle.elm
@@ -5,3 +5,5 @@ convertsStyle = Html.div [Html.Attributes.style [("a", "1")]] []
 convertsExposedStyle = Html.div [style [("a", "1")]] []
 
 doesntConvertOtherFunctions f = Html.div [ f [("a", "1")] ] []
+
+comments = Html.div{-A-}[{-B-}style{-C-}[{-D-}({-E-}"a"{-F-},{-G-}"1"{-H-}){-I-},{-J-}({-K-}"b"{-L-},{-M-}"2"{-N-}){-O-}]{-P-}]{-Q-}[]
--- tests/test-files/transform/Elm-0.19/ConvertHtmlAttributesStyle.formatted.elm
+++ tests/test-files/transform/Elm-0.19/ConvertHtmlAttributesStyle.formatted.elm
@@ -14,3 +14,16 @@ convertsExposedStyle =
 
 doesntConvertOtherFunctions f =
     Html.div [ f [ ( "a", "1" ) ] ] []
+
+
+comments =
+    Html.div
+        {- A -}
+        [ {- B -} {- C -} {- D -} {- O -} style ({- E -} "a" {- F -}) ({- G -} "1" {- H -})
+
+        {- I -}
+        , {- B -} {- C -} {- J -} {- O -} style ({- K -} "b" {- L -}) ({- M -} "2" {- N -})
+
+        {- P -}
+        ]
+        {- Q -} []
