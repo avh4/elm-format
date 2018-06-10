55610c480ca23cb6458dd28e22898358334558ec
Aaron VonderHaar <gruen0aermel@gmail.com>
2018-06-09 22:29:22 -0700

    0.19 upgrade: convert % to modBy

---
+++ tests/test-files/transform/Elm-0.19/ConvertBasicsModBy.formatted.elm
@@ -0,0 +1,45 @@
+module Main exposing (binop, binop_comments, extraArgs, fullyApplied, fullyApplied_comments, looserBinding, looserBinding_comments, partiallyApplied, tigherBinding, tigherBinding_comments, unapplied)
+
+
+binop =
+    modBy 4 -1
+
+
+binop_comments =
+    modBy {- B -} 4 {- A -} -1
+
+
+tigherBinding =
+    modBy (3 ^ 4) (1 ^ 2)
+
+
+tigherBinding_comments =
+    modBy {- D -} (3 {- E -} ^ {- F -} 4) {- C -} (1 {- A -} ^ {- B -} 2)
+
+
+looserBinding =
+    1 + modBy 3 2 + 4
+
+
+looserBinding_comments =
+    1 {- A -} + {- B -} modBy {- D -} 3 {- C -} 2 {- E -} + {- F -} 4
+
+
+fullyApplied =
+    modBy 4 -1
+
+
+fullyApplied_comments =
+    modBy {- B -} 4 {- A -} -1
+
+
+partiallyApplied =
+    \modulus -> modBy modulus 1
+
+
+unapplied =
+    \dividend modulus -> modBy modulus dividend
+
+
+extraArgs =
+    modBy 2 1 3 4
