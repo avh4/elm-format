d1ea19fa7e471f9c2aff03b16889da0e5d358d0e
Aaron VonderHaar <gruen0aermel@gmail.com>
2018-08-17 14:17:34 -0700

    Move literal expression examples to Literals.elm

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -415,6 +415,7 @@ checkUpgrade 0.18 Elm-0.18/RenameTupleFunctions.elm
 checkValidationOutputFormat
 
 checkJson good/AllSyntax/0.18/Expressions.elm
+checkJson good/AllSyntax/0.18/Literals.elm
 checkJson good/json/ExternalReferences.elm
 
 echo
--- tests/test-files/good/AllSyntax/0.18/Literals.elm
+++ tests/test-files/good/AllSyntax/0.18/Literals.elm
@@ -6,9 +6,24 @@ unit =
 
 
 boolean =
-    ( True
+    [ True
     , False
-    )
+    ]
+
+
+integer =
+    [ 1
+    , 0x0F1A
+    , 0x7FFFFFFFFFFFFFFF
+    , 9223372036854775807
+    , 0x8000000000000000
+    , -9223372036854775808
+    ]
+
+
+float =
+    [ 2.0
+    ]
 
 
 string =
@@ -24,10 +39,10 @@ stringWithSpecialEscapedCharacters =
 
 
 stringWithEscapedControlCharacters =
-    ( "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x0B\x0C\x0D\x0E\x0F"
+    [ "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x0B\x0C\x0D\x0E\x0F"
     , "\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F"
     , "\x7F"
-    )
+    ]
 
 
 stringWithWindowsWorkaroundCharacters =
