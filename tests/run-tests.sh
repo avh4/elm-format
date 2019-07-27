5c42d1619b9c705744e8420b870a394854f4171d
Aaron VonderHaar <gruen0aermel@gmail.com>
2019-07-27 12:01:07 -0700

    Literal tabs are now allowed in String and Char literals, as is allowed by Elm 0.19

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -316,6 +316,7 @@ checkGood 0.19 AllSyntax/0.19/Literals.elm
 checkGood 0.19 AllSyntax/0.19/InfixDeclaration.elm
 
 checkTransformation 0.19 Elm-0.19/ExplicitlyListExposedValues.elm
+checkTransformation 0.19 Elm-0.19/ParseTabsInLiterals.elm
 
 checkUpgrade 0.19 Elm-0.19/CommaFunctionsBecomeLambdas.elm
 checkUpgrade 0.19 Elm-0.19/RemoveBangOperator.elm
