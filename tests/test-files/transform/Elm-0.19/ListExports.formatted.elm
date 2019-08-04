523946d86904c34f5d5f40daf699d632be6550d7
Aaron VonderHaar <gruen0aermel@gmail.com>
2019-08-03 17:00:06 -0700

    Copy ListExports test to transform tests

---
+++ tests/test-files/transform/Elm-0.19/ListExports.formatted.elm
@@ -0,0 +1,49 @@
+module Main exposing
+    ( a
+    , X
+    , Y(..)
+    , b, c
+    , p, q
+    )
+
+{-|
+
+@docs a
+
+
+# Heading
+
+@docs X
+@docs Y
+@docs b, c
+
+-}
+
+
+type alias X =
+    ()
+
+
+type Y
+    = Y1
+    | Y2
+
+
+q =
+    4
+
+
+a =
+    1
+
+
+b =
+    2
+
+
+c =
+    3
+
+
+p =
+    5
