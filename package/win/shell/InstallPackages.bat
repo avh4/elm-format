af1299314388d83184e5058fc8583bfb415a23bd
Aaron VonderHaar <gruen0aermel@gmail.com>
2019-08-08 23:14:01 -0700

    Update windows packaging scripts

--- package/win/shell/InstallPackages.bat
+++ package/win/shell/InstallPackages.bat
@@ -1,4 +1,8 @@
 chocolatey feature enable -n=allowGlobalConfirmation
 choco install BoxStarter
 choco install git
+choco install mingw
+choco install python
+choco install zip
+choco install --x86 haskell-stack
 chocolatey feature disable -n=allowGlobalConfirmation
