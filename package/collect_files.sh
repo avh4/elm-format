9c6bb844f46363da6aa5b6afad80dd7d0973f913
Aaron VonderHaar <av@noredink.com>
2020-02-29 09:36:13 -0800

    Update publishing instructions

--- package/collect_files.sh
+++ package/collect_files.sh
@@ -4,9 +4,6 @@ set -ex
 
 VERSION="$(git describe --abbrev=8)"
 
-
-## Collect files
-
 pushd package/win/elm-format
 tar zxvf "elm-format-${VERSION}-win-i386.tgz"
 zip "elm-format-${VERSION}-win-i386.zip" elm-format.exe
@@ -14,8 +11,3 @@ popd
 
 cp -v package/win/elm-format/elm-format-"${VERSION}"-win-i386.zip ./
 
-for i in elm-format-${VERSION}-{mac-x64.tgz,win-i386.zip,linux-x64.tgz}; do
-  keybase pgp sign --detached --infile "$i" --outfile "$i".asc
-  # github-release upload --user avh4 --repo elm-format --tag "$VERSION" --file "$BUILD".tgz
-#   github-release upload --user avh4 --repo elm-format --tag "$VERSION" --file "$BUILD".tgz.asc
-done
