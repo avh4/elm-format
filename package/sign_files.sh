137af1e2cf0fdd00ab798c0c19541064dcb399e0
Aaron VonderHaar <gruen0aermel@gmail.com>
2020-09-23 22:18:42 -0700

    Update packaging scripts for Windows binaries

--- package/sign_files.sh
+++ package/sign_files.sh
@@ -4,7 +4,7 @@ set -exo pipefail
 
 VERSION="$(git describe --abbrev=8)"
 
-for i in dist/elm-format-${VERSION}-{mac-x64.tgz,win-i386.zip,linux-x64.tgz}; do
+for i in dist/elm-format-${VERSION}-{mac-x64.tgz,win-x64.zip,linux-x64.tgz}; do
   keybase pgp sign --detached --infile "$i" --outfile "$i".asc
   # github-release upload --user avh4 --repo elm-format --tag "$VERSION" --file "$BUILD".tgz
   # github-release upload --user avh4 --repo elm-format --tag "$VERSION" --file "$BUILD".tgz.asc
