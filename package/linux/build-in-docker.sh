ac1553f92bce50b3b1e4d942ac77764f1fcce2e5
Aaron VonderHaar <gruen0aermel@gmail.com>
2020-09-23 21:23:40 -0700

    Update linux packaging script

--- package/linux/build-in-docker.sh
+++ package/linux/build-in-docker.sh
@@ -2,5 +2,7 @@
 
 set -ex
 
+./build.sh -- clean
 docker build -t elm-format-dev-linux .
 docker run -v "$(pwd)":/elm-format -w /elm-format elm-format-dev-linux ./package/linux/build-package.sh
+docker run -v "$(pwd)":/elm-format -w /elm-format elm-format-dev-linux ./build.sh -- clean
