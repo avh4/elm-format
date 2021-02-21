ec130eaa341d6c36bae937070e3e27c9124ddf6a
Aaron VonderHaar <gruen0aermel@gmail.com>
2021-02-21 14:52:18 -0800

    JSON-to-Elm: implement import exposing clauses

--- tests/test-files/from-json/Elm-0.19/ModuleImports.elm
+++ tests/test-files/from-json/Elm-0.19/ModuleImports.elm
@@ -1,5 +1,8 @@
 module ModuleImports exposing (f)
 
+import Exposing.Everything exposing (..)
+import Exposing.Types exposing (ExposeAllTags(..), ExposeNoTags)
+import Exposing.Values exposing (a, b)
 import Multi.Word.Alias as WordAlias
 import Multi.Word.NoAlias
 import SingleWordAlias as S
