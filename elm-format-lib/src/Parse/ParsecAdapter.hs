f94b57a07b65cc44681bdff9d54d61d20ce29dfa
emmabastas <emma.bastas@protonmail.com>
2021-07-24 01:05:10 +0200

    Move `ParsecAdapter.Message` into separate module
    Breaks an import cycle

--- elm-format-lib/src/Parse/ParsecAdapter.hs
+++ elm-format-lib/src/Parse/ParsecAdapter.hs
@@ -38,7 +38,7 @@ module Parse.ParsecAdapter
   -- Text.Parsec.Error
   , ParseError
   , newErrorUnknown
-  , Message
+  , module Parse.ParsecAdapter.Message
   , errorPos
   , errorMessages
   -- Text.Parsec.Combinator
@@ -75,6 +75,7 @@ module Parse.ParsecAdapter
   )
   where
 
+import Parse.ParsecAdapter.Message (Message(..))
 import Parse.Primitives (Row, Col)
 import qualified Parse.Primitives as EP
 import Parse.State (State(..))
@@ -371,30 +372,6 @@ instance Show SourcePos where
                           ", column " ++ show column ++
                           ")"
 -- Text.Parsec.Error
-
-
-data Message
-  = SysUnExpect !String -- @ library generated unexpect
-  | UnExpect    !String -- @ unexpected something
-  | Expect      !String -- @ expecting something
-  | Message     !String -- @ raw message
-  deriving ( Typeable )
-
-
-instance Enum Message where
-    fromEnum (SysUnExpect _) = 0
-    fromEnum (UnExpect    _) = 1
-    fromEnum (Expect      _) = 2
-    fromEnum (Message     _) = 3
-    toEnum _ = error "toEnum is undefined for Message"
-
-
-instance Eq Message where
-    m1 == m2 = fromEnum m1 == fromEnum m2
-
-
-instance Ord Message where
-    compare msg1 msg2 = compare (fromEnum msg1) (fromEnum msg2)
 
 
 messageString :: Message -> String
