7d1d6569ff8d31b48df5d8638e710af8060faaf2
emmabastas <emma.bastas@protonmail.com>
2021-08-03 15:05:24 +0200

    Uncomment declarations related to number parsing

--- elm-format-lib/src/Parse/Number.hs
+++ elm-format-lib/src/Parse/Number.hs
@@ -4,12 +4,12 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE BangPatterns, UnboxedTuples #-}
 module Parse.Number
---  ( Number(..)
---  , number
---  , Outcome(..)
---  , chompInt
-  ( chompHex
---  , precedence
+  ( Number(..)
+  , number
+  , Outcome(..)
+  , chompInt
+  , chompHex
+  --, precedence
   )
   where
 
@@ -18,11 +18,11 @@ import Data.Word (Word8)
 import Foreign.Ptr (Ptr, plusPtr, minusPtr)
 
 --import qualified AST.Utils.Binop as Binop
---import qualified Elm.Float as EF
---import Parse.Primitives (Parser, Row, Col)
+import qualified Elm.Float as EF
+import Parse.Primitives (Parser, Row, Col)
 import qualified Parse.Variable as Var
 import qualified Parse.Primitives as P
---import qualified Reporting.Error.Syntax as E
+import qualified Reporting.Error.Syntax as E
 
 
 
@@ -35,218 +35,218 @@ isDirtyEnd pos end word =
 
 
 --{-# INLINE isDecimalDigit #-}
---isDecimalDigit :: Word8 -> Bool
---isDecimalDigit word =
---  word <= 0x39 {-9-} && word >= 0x30 {-0-}
---
---
---
+isDecimalDigit :: Word8 -> Bool
+isDecimalDigit word =
+  word <= 0x39 {-9-} && word >= 0x30 {-0-}
+
+
+
 ---- NUMBERS
---
---
---data Number
---  = Int Int
---  | Float EF.Float
---
---
---number :: (Row -> Col -> x) -> (E.Number -> Row -> Col -> x) -> Parser x Number
---number toExpectation toError =
---  P.Parser $ \(P.State src pos end indent row col) cok _ cerr eerr ->
---    if pos >= end then
---      eerr row col toExpectation
---
---    else
---      let !word = P.unsafeIndex pos in
---      if not (isDecimalDigit word) then
---        eerr row col toExpectation
---
---      else
---        let
---          outcome =
---            if word == 0x30 {-0-} then
---              chompZero (plusPtr pos 1) end
---            else
---              chompInt (plusPtr pos 1) end (fromIntegral (word - 0x30 {-0-}))
---        in
---          case outcome of
---            Err newPos problem ->
---              let
---                !newCol = col + fromIntegral (minusPtr newPos pos)
---              in
---              cerr row newCol (toError problem)
---
---            OkInt newPos n ->
---              let
---                !newCol = col + fromIntegral (minusPtr newPos pos)
---                !integer = Int n
---                !newState = P.State src newPos end indent row newCol
---              in
---              cok integer newState
---
---            OkFloat newPos ->
---              let
---                !newCol = col + fromIntegral (minusPtr newPos pos)
---                !copy = EF.fromPtr pos newPos
---                !float = Float copy
---                !newState = P.State src newPos end indent row newCol
---              in
---              cok float newState
---
---
---
+
+
+data Number
+  = Int Int
+  | Float EF.Float
+
+
+number :: (Row -> Col -> x) -> (E.Number -> Row -> Col -> x) -> Parser x Number
+number toExpectation toError =
+  P.Parser $ \(P.State src pos end indent row col) cok _ cerr eerr ->
+    if pos >= end then
+      eerr row col toExpectation
+
+    else
+      let !word = P.unsafeIndex pos in
+      if not (isDecimalDigit word) then
+        eerr row col toExpectation
+
+      else
+        let
+          outcome =
+            if word == 0x30 {-0-} then
+              chompZero (plusPtr pos 1) end
+            else
+              chompInt (plusPtr pos 1) end (fromIntegral (word - 0x30 {-0-}))
+        in
+          case outcome of
+            Err newPos problem ->
+              let
+                !newCol = col + fromIntegral (minusPtr newPos pos)
+              in
+              cerr row newCol (toError problem)
+
+            OkInt newPos n ->
+              let
+                !newCol = col + fromIntegral (minusPtr newPos pos)
+                !integer = Int n
+                !newState = P.State src newPos end indent row newCol
+              in
+              cok integer newState
+
+            OkFloat newPos ->
+              let
+                !newCol = col + fromIntegral (minusPtr newPos pos)
+                !copy = EF.fromPtr pos newPos
+                !float = Float copy
+                !newState = P.State src newPos end indent row newCol
+              in
+              cok float newState
+
+
+
 ---- CHOMP OUTCOME
+
+
+-- first Int is newPos
 --
---
----- first Int is newPos
-----
---data Outcome
---  = Err (Ptr Word8) E.Number
---  | OkInt (Ptr Word8) Int
---  | OkFloat (Ptr Word8)
---
---
---
----- CHOMP INT
---
---
---chompInt :: Ptr Word8 -> Ptr Word8 -> Int -> Outcome
---chompInt !pos end !n =
---  if pos >= end then
---
---    OkInt pos n
---
---  else
---
---    let
---      !word = P.unsafeIndex pos
---    in
---      if isDecimalDigit word then
---        chompInt (plusPtr pos 1) end (10 * n + fromIntegral (word - 0x30 {-0-}))
---
---      else if word == 0x2E {-.-} then
---        chompFraction pos end n
---
---      else if word == 0x65 {-e-} || word == 0x45 {-E-} then
---        chompExponent (plusPtr pos 1) end
---
---      else if isDirtyEnd pos end word then
---        Err pos E.NumberEnd
---
---      else
---        OkInt pos n
---
---
---
----- CHOMP FRACTION
---
---
---chompFraction :: Ptr Word8 -> Ptr Word8 -> Int -> Outcome
---chompFraction pos end n =
---  let
---    !pos1 = plusPtr pos 1
---  in
---  if pos1 >= end then
---    Err pos (E.NumberDot n)
---
---  else if isDecimalDigit (P.unsafeIndex pos1) then
---    chompFractionHelp (plusPtr pos1 1) end
---
---  else
---    Err pos (E.NumberDot n)
---
---
---chompFractionHelp :: Ptr Word8 -> Ptr Word8 -> Outcome
---chompFractionHelp pos end =
---  if pos >= end then
---    OkFloat pos
---
---  else
---    let !word = P.unsafeIndex pos in
---    if isDecimalDigit word then
---      chompFractionHelp (plusPtr pos 1) end
---
---    else if word == 0x65 {-e-} || word == 0x45 {-E-} then
---      chompExponent (plusPtr pos 1) end
---
---    else if isDirtyEnd pos end word then
---      Err pos E.NumberEnd
---
---    else
---      OkFloat pos
---
---
---
----- CHOMP EXPONENT
---
---
---chompExponent :: Ptr Word8 -> Ptr Word8 -> Outcome
---chompExponent pos end =
---  if pos >= end then
---    Err pos E.NumberEnd
---
---  else
---    let !word = P.unsafeIndex pos in
---    if isDecimalDigit word then
---      chompExponentHelp (plusPtr pos 1) end
---
---    else if word == 0x2B {-+-} || word == 0x2D {---} then
---
---      let !pos1 = plusPtr pos 1 in
---      if pos1 < end && isDecimalDigit (P.unsafeIndex pos1) then
---        chompExponentHelp (plusPtr pos 2) end
---      else
---        Err pos E.NumberEnd
---
---    else
---      Err pos E.NumberEnd
---
---
---chompExponentHelp :: Ptr Word8 -> Ptr Word8 -> Outcome
---chompExponentHelp pos end =
---  if pos >= end then
---    OkFloat pos
---
---  else if isDecimalDigit (P.unsafeIndex pos) then
---    chompExponentHelp (plusPtr pos 1) end
---
---  else
---    OkFloat pos
---
---
---
----- CHOMP ZERO
---
---
---chompZero :: Ptr Word8 -> Ptr Word8 -> Outcome
---chompZero pos end =
---  if pos >= end then
---    OkInt pos 0
---
---  else
---    let !word = P.unsafeIndex pos in
---    if word == 0x78 {-x-} then
---      chompHexInt (plusPtr pos 1) end
---
---    else if word == 0x2E {-.-} then
---      chompFraction pos end 0
---
---    else if isDecimalDigit word then
---      Err pos E.NumberNoLeadingZero
---
---    else if isDirtyEnd pos end word then
---      Err pos E.NumberEnd
---
---    else
---      OkInt pos 0
---
---
---chompHexInt :: Ptr Word8 -> Ptr Word8 -> Outcome
---chompHexInt pos end =
---  let (# newPos, answer #) = chompHex pos end in
---  if answer < 0 then
---    Err newPos E.NumberHexDigit
---  else
---    OkInt newPos answer
+data Outcome
+  = Err (Ptr Word8) E.Number
+  | OkInt (Ptr Word8) Int
+  | OkFloat (Ptr Word8)
+
+
+
+-- CHOMP INT
+
+
+chompInt :: Ptr Word8 -> Ptr Word8 -> Int -> Outcome
+chompInt !pos end !n =
+  if pos >= end then
+
+    OkInt pos n
+
+  else
+
+    let
+      !word = P.unsafeIndex pos
+    in
+      if isDecimalDigit word then
+        chompInt (plusPtr pos 1) end (10 * n + fromIntegral (word - 0x30 {-0-}))
+
+      else if word == 0x2E {-.-} then
+        chompFraction pos end n
+
+      else if word == 0x65 {-e-} || word == 0x45 {-E-} then
+        chompExponent (plusPtr pos 1) end
+
+      else if isDirtyEnd pos end word then
+        Err pos E.NumberEnd
+
+      else
+        OkInt pos n
+
+
+
+-- CHOMP FRACTION
+
+
+chompFraction :: Ptr Word8 -> Ptr Word8 -> Int -> Outcome
+chompFraction pos end n =
+  let
+    !pos1 = plusPtr pos 1
+  in
+  if pos1 >= end then
+    Err pos (E.NumberDot n)
+
+  else if isDecimalDigit (P.unsafeIndex pos1) then
+    chompFractionHelp (plusPtr pos1 1) end
+
+  else
+    Err pos (E.NumberDot n)
+
+
+chompFractionHelp :: Ptr Word8 -> Ptr Word8 -> Outcome
+chompFractionHelp pos end =
+  if pos >= end then
+    OkFloat pos
+
+  else
+    let !word = P.unsafeIndex pos in
+    if isDecimalDigit word then
+      chompFractionHelp (plusPtr pos 1) end
+
+    else if word == 0x65 {-e-} || word == 0x45 {-E-} then
+      chompExponent (plusPtr pos 1) end
+
+    else if isDirtyEnd pos end word then
+      Err pos E.NumberEnd
+
+    else
+      OkFloat pos
+
+
+
+-- CHOMP EXPONENT
+
+
+chompExponent :: Ptr Word8 -> Ptr Word8 -> Outcome
+chompExponent pos end =
+  if pos >= end then
+    Err pos E.NumberEnd
+
+  else
+    let !word = P.unsafeIndex pos in
+    if isDecimalDigit word then
+      chompExponentHelp (plusPtr pos 1) end
+
+    else if word == 0x2B {-+-} || word == 0x2D {---} then
+
+      let !pos1 = plusPtr pos 1 in
+      if pos1 < end && isDecimalDigit (P.unsafeIndex pos1) then
+        chompExponentHelp (plusPtr pos 2) end
+      else
+        Err pos E.NumberEnd
+
+    else
+      Err pos E.NumberEnd
+
+
+chompExponentHelp :: Ptr Word8 -> Ptr Word8 -> Outcome
+chompExponentHelp pos end =
+  if pos >= end then
+    OkFloat pos
+
+  else if isDecimalDigit (P.unsafeIndex pos) then
+    chompExponentHelp (plusPtr pos 1) end
+
+  else
+    OkFloat pos
+
+
+
+-- CHOMP ZERO
+
+
+chompZero :: Ptr Word8 -> Ptr Word8 -> Outcome
+chompZero pos end =
+  if pos >= end then
+    OkInt pos 0
+
+  else
+    let !word = P.unsafeIndex pos in
+    if word == 0x78 {-x-} then
+      chompHexInt (plusPtr pos 1) end
+
+    else if word == 0x2E {-.-} then
+      chompFraction pos end 0
+
+    else if isDecimalDigit word then
+      Err pos E.NumberNoLeadingZero
+
+    else if isDirtyEnd pos end word then
+      Err pos E.NumberEnd
+
+    else
+      OkInt pos 0
+
+
+chompHexInt :: Ptr Word8 -> Ptr Word8 -> Outcome
+chompHexInt pos end =
+  let (# newPos, answer #) = chompHex pos end in
+  if answer < 0 then
+    Err newPos E.NumberHexDigit
+  else
+    OkInt newPos answer
 
 
 
