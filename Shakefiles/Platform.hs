535866ba69b97eb1e5755191753baafa3a21ef5d
Taylor Fausak <taylor@fausak.me>
2022-09-21 09:10:07 -0500

    Allow building on Linux ARM64

--- Shakefiles/Platform.hs
+++ Shakefiles/Platform.hs
@@ -3,12 +3,13 @@ module Shakefiles.Platform (Platform(..), Shakefiles.Platform.all, platform, zip
 import qualified System.Info
 
 
-data Platform = Linux | Mac | Windows
+data Platform = Linux | Mac | Windows | LinuxAarch64
 
 instance Show Platform where
     show Linux = "linux-x64"
     show Mac = "mac-x64"
     show Windows = "win-x64"
+    show LinuxAarch64 = "linux-aarch64"
 
 
 all :: [Platform]
@@ -16,6 +17,7 @@ all =
     [ Linux
     , Mac
     , Windows
+    , LinuxAarch64
     ]
 
 
@@ -27,6 +29,7 @@ platform =
         ("osx", "x86_64") -> Mac
         ("mingw32", "x86_64") -> Windows
         ("win32", "x86_64") -> Windows
+        ("linux", "aarch64") -> LinuxAarch64
         other -> error ("unhandled operating system: " ++ show other)
 
 
@@ -35,6 +38,7 @@ zipFormatFor = \case
     Linux -> "tgz"
     Mac -> "tgz"
     Windows -> "zip"
+    LinuxAarch64 -> "tgz"
 
 
 binExt :: Platform -> String
@@ -42,6 +46,7 @@ binExt = \case
     Linux -> ""
     Mac -> ""
     Windows -> ".exe"
+    LinuxAarch64 -> ""
 
 
 githubRunnerOs :: Platform -> String
@@ -49,6 +54,7 @@ githubRunnerOs = \case
     Linux -> "Linux"
     Windows -> "Windows"
     Mac -> "macOS"
+    LinuxAarch64 -> "Linux"
 
 
 cabalInstallOs :: String
