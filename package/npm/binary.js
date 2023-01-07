cb0915e9556c4ce5e683a4dbb01fcac28d6dc7cf
Simon Lydell <simon.lydell@gmail.com>
2023-01-07 15:02:59 +0100

    Fix binaryPath

--- package/npm/binary.js
+++ package/npm/binary.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
-var package = require("./package.json");
 var path = require("path");
+var package = require("./package.json");
 
 module.exports = function () {
   var os = process.env.BINWRAP_PLATFORM || process.platform;
@@ -52,7 +52,7 @@ module.exports = function () {
     return subBinaryPath;
   }
 
-  var binaryPath = path.resolve(__dirname, package.bin.elm);
+  var binaryPath = path.resolve(__dirname, package.bin["elm-format"]);
   var tmpPath = binaryPath + ".tmp";
 
   try {
