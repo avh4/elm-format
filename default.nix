d07ae532ef56d874d4ab2c711c2f67416f370e17
Aaron VonderHaar <gruen0aermel@gmail.com>
2023-02-19 19:45:16 -0800

    Improve nix caching with gitignore.nix

--- default.nix
+++ default.nix
@@ -1,6 +1,9 @@
 { sources ? import ./nix/sources.nix, compiler ? "ghc925" }:
 
 let
+  gitignore = import sources."gitignore.nix" { };
+  inherit (gitignore) gitignoreSource gitignoreFilterWith;
+
   haskellPackageOverrides = pkgs: self: super:
     with pkgs.haskell.lib; rec {
       hedgehog = self.hedgehog_1_2;
@@ -11,25 +14,49 @@ let
     };
 
   elmFormatPackages = self: super:
+    with pkgs.haskell.lib;
     let
+      inherit (pkgs) lib;
+
       aeson = self.aeson_2_1_1_0;
       text = self.text_2_0_1;
+
+      mkPkg = name: path: args:
+        overrideCabal (self.callCabal2nix name path args) (orig: {
+          src = lib.cleanSourceWith {
+            name = "source";
+            filter = fpath: ftype:
+              if builtins.elem fpath [
+                (toString ./generated/Build_elm_format.hs)
+                (toString ./generated)
+              ] then
+              # Include certain gitignored files
+                true
+              else if path == ./. && builtins.elem fpath [
+                (toString ./avh4-lib)
+                (toString ./elm-format-lib)
+                (toString ./elm-format-test-lib)
+                (toString ./elm-format-markdown)
+              ] then
+              # Exclude subprojects when building the top-level package
+                false
+              else
+                gitignoreFilterWith { basePath = path; } fpath ftype;
+            src = path;
+          };
+        });
     in {
-      elm-format-build = self.callCabal2nix "elm-format-build" ./Shakefile { };
-      elm-format = self.callCabal2nix "elm-format" ./. { inherit aeson text; };
-      avh4-lib = self.callCabal2nix "avh4-lib" ./avh4-lib { inherit text; };
-      elm-format-lib = self.callCabal2nix "elm-format-lib" ./elm-format-lib {
-        inherit aeson text;
+      elm-format-build = mkPkg "elm-format-build" ./Shakefile { };
+      elm-format = mkPkg "elm-format" ./. { inherit aeson text; };
+      avh4-lib = mkPkg "avh4-lib" ./avh4-lib { inherit text; };
+      elm-format-lib =
+        mkPkg "elm-format-lib" ./elm-format-lib { inherit aeson text; };
+      elm-format-test-lib = mkPkg "elm-format-test-lib" ./elm-format-test-lib {
+        inherit text;
+        hspec-golden = self.hspec-golden_0_1_0_3;
       };
-      elm-format-test-lib =
-        self.callCabal2nix "elm-format-test-lib" ./elm-format-test-lib {
-          inherit text;
-          hspec-golden = self.hspec-golden_0_1_0_3;
-        };
       elm-format-markdown =
-        self.callCabal2nix "elm-format-markdown" ./elm-format-markdown {
-          inherit text;
-        };
+        mkPkg "elm-format-markdown" ./elm-format-markdown { inherit text; };
     };
 
   pkgs = import sources.nixpkgs {
