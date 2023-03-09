d01ef31a99241bfa8a909f4bb624db4ce7d5bc71
Aaron VonderHaar <gruen0aermel@gmail.com>
2023-03-09 11:06:28 -0800

    Use hpack for elm-format-test-lib

--- dev/build.sh
+++ dev/build.sh
@@ -5,4 +5,5 @@ set -euo pipefail
 (cd avh4-lib && hpack)
 (cd elm-format-markdown && hpack)
 (cd elm-format-lib && hpack)
+(cd elm-format-test-lib && hpack)
 exec cabal run build-elm-format:exe:build-elm-format -- --share "$@"
